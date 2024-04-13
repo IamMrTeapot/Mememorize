009336f0b4f6e08edf915f912c77c811d5b8d49a
its144prelationship <99236505+its144prelationship@users.noreply.github.com>
2024-04-13 14:57:09 +0700

    PP-home page (#1)

---
+++ src/components/LoginButton.tsx
@@ -0,0 +1,28 @@
+interface LoginButtonProps {
+  onClick: () => void;
+  color: "green" | "yellow" | "red" | "white";
+  isRegister?: boolean;
+}
+
+export default function LoginButton({
+  onClick,
+  color,
+  isRegister,
+}: LoginButtonProps) {
+  const bgColor =
+    color === "green"
+      ? "bg-[#0EAA1D]"
+      : color === "yellow"
+        ? "bg-[#E3E800]"
+        : color === "red"
+          ? "bg-[#AA0E0E]"
+          : "bg-[#FFFFFF]";
+  return (
+    <button
+      className={`${bgColor} text-sm ${color === "white" ? "text-black border-[1px] border-black" : "text-white"} font-urbanist font-medium mx-6 my-2 p-3 rounded-lg hover:scale-[102%] hover:shadow-md duration-300 ease-in-out`}
+      onClick={onClick}
+    >
+      {isRegister ? "Register" : "Login"}
+    </button>
+  );
+}
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,3 +1,22 @@
+import MainMeme from "../../assets/main-meme.svg";
+import LoginButton from "../../components/LoginButton";
 export default function HomePage() {
-  return <div>Home</div>;
+  return (
+    <main className="w-full bg-meme-yellow min-h-[100vh] flex justify-center">
+    <div className="flex flex-wrap justify-evenly items-center">
+      <img src={MainMeme} alt="Main Meme" className="w-[350px] mt-12" />
+      <div className="flex flex-col my-10 mx-10">
+        <h1 className="font-urbanist font-extrabold text-4xl m-6">
+          MEMEMORIZE
+        </h1>
+        <p className="font-urbanist font-light text-base text-center leading-6 mb-5">
+          เรียนคลาวมีแต่เรื่องซีเรียส
+          <br />
+          มานั่งดูมีมคลายเครียดดีกว่า
+        </p>
+        <LoginButton onClick={()=>{}} color="green"/>
+        <LoginButton onClick={()=>{}} color="white" isRegister={true}/>
+      </div>
+    </div></main>
+  );
 }
