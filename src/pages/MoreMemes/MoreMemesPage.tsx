432f3aafee2121a8a01e90967fa9707ac309d3ba
its144prelationship <99236505+its144prelationship@users.noreply.github.com>
2024-04-13 16:53:44 +0700

    PP-more memes page (#4)

--- src/pages/MoreMemes/MoreMemesPage.tsx
+++ src/pages/MoreMemes/MoreMemesPage.tsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+import UploadMeme from "./components/UploadMeme";
+import SuccessfulUploaded from "./components/SuccessfulUploaded";
+import NavBar from "../../components/NavBar";
 export default function MoreMemesPage() {
-  return <div>MoreMemes</div>;
+  const [isUploaded, setIsUploaded] = useState(true);
+  return (
+    <main className="w-full bg-meme-yellow min-h-[100vh]">
+      <div className="flex flex-col justify-center items-center space-y-4">
+        <NavBar name="Mememorize" />
+        {!isUploaded ? (
+          <UploadMeme setIsUploaded={setIsUploaded} />
+        ) : (
+          <SuccessfulUploaded />
+        )}
+      </div>
+    </main>
+  );
 }
