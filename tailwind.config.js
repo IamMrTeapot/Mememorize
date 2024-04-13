a031821a1e79a8beea00e5a1833dd0f38fff6828
Sirawit Pulketkij <sirawit.pulketkij@gmail.com>
2024-04-13 15:34:23 +0700

    add login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       colors: {
         "meme-yellow": "#f4f1bb",
         "meme-header": "#f2ed8e",
+        "answer-blue": "#35C2C1",
       },
       fontFamily: {
         urbanist: ["Urbanist", "sans-serif", "sans-francisco"],
