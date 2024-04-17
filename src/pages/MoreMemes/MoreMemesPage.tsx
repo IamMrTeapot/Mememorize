f4e1357c823f8514cd94b441ddb153e5d5bbc0e5
its144prelationship <pastalonely@gmail.com>
2024-04-18 01:47:40 +0700

    feat: connect with backend api

--- src/pages/MoreMemes/MoreMemesPage.tsx
+++ src/pages/MoreMemes/MoreMemesPage.tsx
@@ -3,13 +3,15 @@ import UploadMeme from "./components/UploadMeme";
 import SuccessfulUploaded from "./components/SuccessfulUploaded";
 import NavBar from "../../components/NavBar";
 export default function MoreMemesPage() {
-  const [isUploaded, setIsUploaded] = useState(true);
+  const [isUploaded, setIsUploaded] = useState(false);
+  const username = "Mememorize";
+  const userid = "1234455678";
   return (
     <main className="w-full bg-meme-yellow min-h-[100vh]">
       <div className="flex flex-col justify-center items-center space-y-4">
-        <NavBar name="Mememorize" />
+        <NavBar name={username} />
         {!isUploaded ? (
-          <UploadMeme setIsUploaded={setIsUploaded} />
+          <UploadMeme setIsUploaded={setIsUploaded} username={username} userid={userid}/>
         ) : (
           <SuccessfulUploaded />
         )}
